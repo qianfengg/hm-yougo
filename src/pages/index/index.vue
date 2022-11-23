d7fc2951f82c6a04fdb8df550836af1a4f120bf7
qianfengg <fred_gqf0221@163.com>
2022-11-23 16:12:50 +0800

    安装uni-ui

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -8,6 +8,11 @@
       父
       <view class="son">儿</view>
     </view>
+    <uni-card>
+      <text
+        >这是一个基础卡片示例，内容较少，此示例展示了一个没有任何属性不带阴影的卡片。</text
+      >
+    </uni-card>
   </view>
 </template>
 
