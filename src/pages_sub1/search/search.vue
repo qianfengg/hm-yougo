3d847905d2107bba8f09aec2f7bd587caf5aa32a
qianfengg <fred_gqf0221@163.com>
2022-11-25 09:57:37 +0800

    搜索建议列表渲染并跳转

--- src/pages_sub1/search/search.vue
+++ src/pages_sub1/search/search.vue
@@ -8,6 +8,18 @@
         @input="inputHandler"
       ></uni-search-bar>
     </view>
+    <!-- 搜索建议列表 -->
+    <view class="sugg-list">
+      <view
+        class="sugg-item"
+        v-for="(item, index) in suggestions"
+        :key="index"
+        @click="gotoProductDetail(item)"
+      >
+        <view class="goods-name">{{ item.goods_name }}</view>
+        <uni-icons type="arrowright" size="16"></uni-icons>
+      </view>
+    </view>
   </view>
 </template>
 
@@ -21,6 +33,11 @@ export default {
     };
   },
   methods: {
+    gotoProductDetail(item) {
+      uni.navigateTo({
+        url: `/pages_sub1/product-detail/product-detail?pid=${item.goods_id}`,
+      });
+    },
     inputHandler(value) {
       clearTimeout(this.timer);
       this.timer = setTimeout(() => {
@@ -29,7 +46,10 @@ export default {
       }, 500);
     },
     async getSuggestions() {
-      if (!this.keyword) return;
+      if (!this.keyword) {
+        this.suggestions = [];
+        return;
+      }
       const { meta, message } = await this.$http.get("/goods/qsearch", {
         query: this.keyword,
       });
@@ -54,5 +74,31 @@ export default {
   position: sticky;
   top: 0;
   z-index: 999;
+}
+// 搜索建议列表
+.sugg-list {
+  padding: 0 10rpx;
+
+  .sugg-item {
+    font-size: 24rpx;
+    padding: 26rpx 0;
+    border-bottom: 1rpx solid #efefef;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    .goods-name {
+      margin-right: 6rpx;
+
+      // 溢出部分隐藏
+      overflow: hidden;
+
+      // 文字不允许换行
+      white-space: nowrap;
+
+      // 文字溢出后使用省略号表示
+      text-overflow: ellipsis;
+    }
+  }
 }
 </style>
